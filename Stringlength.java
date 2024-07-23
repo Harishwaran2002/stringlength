3f7b139894facbd95b5827c50a57f820ae0472b7
Harishwaran2002 <146076518+Harishwaran2002@users.noreply.github.com>
2024-07-23 14:17:42 +0530

    Stringlength.java

---
+++ Stringlength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String[]args){
+        String n="madam";
+        int count=0;
+        for(char i:n.toCharArray())
+        {
+            count++;
+        }
+        System.out.println("The String Length:"+count);
+    }
+}
